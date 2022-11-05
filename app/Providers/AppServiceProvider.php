c900f49c56f049a63e91407f497f9b17b1f848dd
klisostom <klis@pm.me>
2022-11-05 08:55:23 -0300

    chore(api): Configurando a chave da api externa

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::share('api_key', '7b652dcc6aa9536f7843b1e5f48973d7');
     }
 }
