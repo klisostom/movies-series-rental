1bc565e3abd27872adb46dd348614833c8f3f1fd
klisostom <klis@pm.me>
2022-11-08 11:57:31 -0300

    feat: Alterando o layout e organizando head e footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,31 +2,20 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-    <!--Import Google Icon Font-->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-    <!-- Roboto -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Serif:opsz@8..144&display=swap" rel="stylesheet">
-
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
-    <style>
-        body {
-            font-family: 'Roboto Serif', serif;
-        }
-    </style>
+    @include('includes.head')
 </head>
 
 <body class="container">
-    @extends('home')
+    <header class="row">
+        @include('includes.search')
+    </header>
+    <main>
+        @yield('content')
+    </main>
 
+    <footer style="position: absolute;bottom: 0; height: 50px;">
+        @include('includes.footer')
+    </footer>
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
