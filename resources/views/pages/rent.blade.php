f2ac8e73a1fd101b5bc72e4e0c770f9a68629d87
klisostom <klis@pm.me>
2022-11-09 14:30:20 -0300

    feat: Novo arquivo view

---
+++ resources/views/pages/rent.blade.php
@@ -0,0 +1,38 @@
+@extends('welcome')
+@section('content')
+    <div class="row">
+        <div class="col s12 m4">
+            <div class="card">
+                <div class="card-image">
+                    <img src="{{ $data['img'] }}" alt="movie photo poster" width="auto" height="300">
+                </div>
+
+                <form action="" class="col s12">
+                    <div class="card-content">
+                        <h5 class="card-title">{{ $data['title'] }}</h5>
+                        <div class="row">
+                            <div class="input-field col s6">
+                                <input id="first_name" name="first_name" type="text" class="validate">
+                                <label for="first_name">Primeiro Nome</label>
+                            </div>
+                            <div class="input-field col s6">
+                                <input id="email" name="email" type="email" class="validate">
+                                <label for="email">Email</label>
+                            </div>
+
+                            <button
+                                class="btn waves-effect waves-light"
+                                type="submit"
+                                id="btnRent"
+                                name="btnRent"
+                                value=""
+                            >
+                                Alugar
+                            </button>
+                            <a href="/" class="waves-effect waves red lighten-5 btn" style="color: black">Voltar</a>
+                        </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@stop
