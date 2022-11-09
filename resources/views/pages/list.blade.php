207afff5dc8db39edc054cbae40d329661209074
klisostom <klis@pm.me>
2022-11-09 11:26:18 -0300

    feat: Adicionando botão para alugar filme

--- resources/views/pages/list.blade.php
+++ resources/views/pages/list.blade.php
@@ -1,21 +1,46 @@
 @extends('welcome')
 @section('content')
-    <ul class="collection">
-        @forelse ($data['results'] as $items)
-            <li class="collection-item avatar">
-                <img src="https://image.tmdb.org/t/p/original/{{ $items['poster_path'] }}" alt="Foto" class="circle"
-                    width="20" height="30">
-                <b class="title">{{ $items['original_title'] }}</b>
-                <br>
-                <p>
-                    {{ $items['overview'] }}
-                </p>
+    <form>
+        @csrf
+        <ul class="collection">
+            @forelse ($data['results'] as $key => $items)
+                <li class="collection-item avatar" value="{{$key}}">
+                    <img src="https://image.tmdb.org/t/p/original{{ $items['poster_path'] }}" alt="Foto" class="circle"
+                        width="20" height="30">
 
-                <h6><b>Lançamento: </b>{{ date_format(date_create($items['release_date']), 'd/m/Y') }}</h6>
-                <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
-            </li>
-        @empty
-            <h1>Not found</h1>
-        @endforelse
-    </ul>
+                    <input
+                        type="hidden"
+                        id="imgPoster{{$key}}"
+                        name="imgPoster{{$key}}"
+                        value="https://image.tmdb.org/t/p/original{{ $items['poster_path'] }}">
+
+                    <b class="title">{{ $items['original_title'] }}</b>
+                    <input
+                        type="hidden"
+                        id="titleFilm{{$key}}"
+                        name="titleFilm"
+                        value="{{ $items['original_title'] }}"
+                    <br>
+                    <p>
+                        {{ $items['overview'] }}
+                    </p>
+
+                    <h6><b>Lançamento: </b>{{ date_format(date_create($items['release_date']), 'd/m/Y') }}</h6>
+
+                    <button
+                        class="btn waves-effect waves-light"
+                        type="button"
+                        id="btnRent{{$key}}"
+                        name="btnRent"
+                        value="{{json_encode([
+                            'title' => $items['original_title'],
+                            'img' => "https://image.tmdb.org/t/p/original".$items['poster_path'],
+                        ])}}"
+                    >Alugar</button>
+                </li>
+            @empty
+                <h1>Not found</h1>
+            @endforelse
+        </ul>
+    </form>
 @stop
